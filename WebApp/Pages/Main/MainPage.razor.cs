bbc2dbea74f0ae3a60eb4f9ff405c73ebff8777e
Alexander Dulemba <olekdulemba@gmail.com>
2025-08-06 20:10:25 -0400

    fixed some layout issues, and got nav animation working with JS

---
+++ WebApp/Pages/Main/MainPage.razor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace WebApp.Pages.Main;
+
+public partial class MainPage : IDisposable
+{
+    [Inject] private ILogger<MainPage> Logger { get; set; } = default!;
+
+    private SetupJsModule.JS? Module { get; set; }
+
+    private async Task SetupJsAnimationsAsync()
+    {
+        if (!OperatingSystem.IsBrowser()) return;
+
+        Module = await SetupJsModule.ImportAsync(Logger);
+        Module?.Setup();
+    }
+
+    [SupportedOSPlatform("browser")]
+    internal partial class SetupJsModule
+    {
+        private const string ModuleName = "module";
+        private const string JsFilePath = "../Pages/Main/MainPage.razor.js";
+
+        internal static async Task<JS?> ImportAsync(ILogger<MainPage> logger, CancellationToken token = default)
+        {
+            try
+            {
+                _ = await JSHost.ImportAsync(ModuleName, JsFilePath, token);
+                return new JS();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Failed to load JS module for MainPage.razor: {ex}", ex);
+                return null;
+            }
+        }
+
+        [JSImport(functionName: "setupScrollingAnimation", ModuleName)]
+        private static partial void Setup();
+
+        [JSImport(functionName: "teardownScrollingAnimation", ModuleName)]
+        private static partial void Dispose();
+
+        internal sealed class JS : IDisposable
+        {
+            internal void Setup() => SetupJsModule.Setup();
+
+            public void Dispose() => SetupJsModule.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        if (OperatingSystem.IsBrowser()) 
+            Module?.Dispose();
+    }
+}
---
+++ WebApp/Pages/Main/MainPage.razor.js
@@ -0,0 +1,81 @@
+﻿export function setupScrollingAnimation() {
+    const scrollContainer = document.getElementById("main-scrolling-content");
+    scrollContainer.addEventListener("scroll", handleScroll);
+    window.scrollContainers = new ScrollContainerData(
+        scrollContainer,
+        document.querySelector(".home-link"),
+        document.querySelector(".projects-link"),
+        document.querySelector(".about-me-link")
+    );
+}
+
+export function teardownScrollingAnimation() {
+    const scrollContainer = document.getElementById("main-scrolling-content");
+    scrollContainer.removeEventListener("scroll", handleScroll);
+    window.ScrollContainers = {};
+}
+
+class ScrollContainerData {
+    static activeClassName = "current";
+    container;
+    homeLink;
+    projectsLink;
+    aboutMeLink;
+
+    /**
+     * 
+     * @param {HTMLElement} container
+     * @param {HTMLElement} homeLink
+     * @param {HTMLElement} projectsLink
+     * @param {HTMLElement} aboutMeLink
+     */
+    constructor(container, homeLink, projectsLink, aboutMeLink) {
+        this.container = container;
+        this.homeLink = homeLink;
+        this.projectsLink = projectsLink;
+        this.aboutMeLink = aboutMeLink;
+    }
+}
+
+/**
+ * 
+ * @param {Event} event
+ */
+function handleScroll(event) {
+    /** @type {ScrollContainerData} */
+    let data = window.scrollContainers;
+    if (data === null || !(data instanceof ScrollContainerData) || data.container === null) return;
+
+    console.log("animation");
+    const mainScrollContainer = data.container;
+    const scrollableHeight = mainScrollContainer.scrollHeight - mainScrollContainer.clientHeight;
+    if (scrollableHeight > 0) {
+        let scrollPercentage = (mainScrollContainer.scrollTop / scrollableHeight) * 100;
+        if (isBetween(0, 33, scrollPercentage)) {
+            data.homeLink.classList.add(ScrollContainerData.activeClassName);
+            data.projectsLink.classList.remove(ScrollContainerData.activeClassName);
+            data.aboutMeLink.classList.remove(ScrollContainerData.activeClassName);
+        }
+        else if (isBetween(33, 66, scrollPercentage)) {
+            data.homeLink.classList.remove(ScrollContainerData.activeClassName);
+            data.projectsLink.classList.add(ScrollContainerData.activeClassName);
+            data.aboutMeLink.classList.remove(ScrollContainerData.activeClassName);
+        }
+        else {
+            data.homeLink.classList.remove(ScrollContainerData.activeClassName);
+            data.projectsLink.classList.remove(ScrollContainerData.activeClassName);
+            data.aboutMeLink.classList.add(ScrollContainerData.activeClassName);
+        }
+    }
+}
+
+/**
+ * 
+ * @param {number} min
+ * @param {number} max
+ * @param {number} num
+ * @returns {boolean}
+ */
+function isBetween(min, max, num) {
+    return num >= min && num <= max;
+}
