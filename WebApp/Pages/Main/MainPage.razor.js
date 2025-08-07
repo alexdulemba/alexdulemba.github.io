e664e2f927ef30aaa25a1de77e6661bffa3323b3
Alexander Dulemba <olekdulemba@gmail.com>
2025-08-07 15:10:40 -0400

    - fixed mobile layout - added mobile navigation animation - added subtle background animation on scroll

--- WebApp/Pages/Main/MainPage.razor.js
+++ WebApp/Pages/Main/MainPage.razor.js
@@ -3,9 +3,9 @@
     scrollContainer.addEventListener("scroll", handleScroll);
     window.scrollContainers = new ScrollContainerData(
         scrollContainer,
-        document.querySelector(".home-link"),
-        document.querySelector(".projects-link"),
-        document.querySelector(".about-me-link")
+        document.querySelectorAll(".home-link"),
+        document.querySelectorAll(".projects-link"),
+        document.querySelectorAll(".about-me-link")
     );
     window.downloadFileFromStream = downloadFileFromStream;
 }
@@ -18,24 +18,89 @@ export function dispose() {
 }
 
 class ScrollContainerData {
-    static activeClassName = "current";
+    static #activeClass = "current";
+    static #scrollBehavior = {
+        behavior: 'smooth',
+        block: 'nearest'
+    }
+    static #bodyGridSizeProperty = "--grid-box-size";
+    body;
     container;
-    homeLink;
-    projectsLink;
-    aboutMeLink;
+    homeLinks;
+    projectsLinks;
+    aboutMeLinks;
 
     /**
-     * 
      * @param {HTMLElement} container
-     * @param {HTMLElement} homeLink
-     * @param {HTMLElement} projectsLink
-     * @param {HTMLElement} aboutMeLink
+     * @param {NodeListOf<Element>} homeLinks
+     * @param {NodeListOf<Element>} projectsLinks
+     * @param {NodeListOf<Element>} aboutMeLinks
      */
-    constructor(container, homeLink, projectsLink, aboutMeLink) {
+    constructor(container, homeLinks, projectsLinks, aboutMeLinks) {
+        this.body = document.getElementsByTagName("body")[0];
         this.container = container;
-        this.homeLink = homeLink;
-        this.projectsLink = projectsLink;
-        this.aboutMeLink = aboutMeLink;
+        this.homeLinks = homeLinks;
+        this.projectsLinks = projectsLinks;
+        this.aboutMeLinks = aboutMeLinks;
+    }
+
+    setHomeLinksToActive() {
+        this.addClassToElements(this.homeLinks, ScrollContainerData.#activeClass);
+        this.removeClassFromElements(this.projectsLinks, ScrollContainerData.#activeClass);
+        this.removeClassFromElements(this.aboutMeLinks, ScrollContainerData.#activeClass);
+    }
+
+    setProjectsLinksToActive() {
+        this.removeClassFromElements(this.homeLinks, ScrollContainerData.#activeClass);
+        this.addClassToElements(this.projectsLinks, ScrollContainerData.#activeClass);
+        this.removeClassFromElements(this.aboutMeLinks, ScrollContainerData.#activeClass);
+    }
+
+    setAboutMeLinksToActive() {
+        this.removeClassFromElements(this.homeLinks, ScrollContainerData.#activeClass);
+        this.removeClassFromElements(this.projectsLinks, ScrollContainerData.#activeClass);
+        this.addClassToElements(this.aboutMeLinks, ScrollContainerData.#activeClass);
+    }
+
+    /**
+     * @param {number} value
+     */
+    setBodyGridSize(value) {
+        var mapped = map(0, 100, 2, 4, value);
+        this.body.style.setProperty(ScrollContainerData.#bodyGridSizeProperty, `max(${mapped}dvw, 8px)`);
+    }
+
+    /**
+     * @param {NodeListOf<Element>} elements
+     * @param {string} className
+     */
+    addClassToElements(elements, className) {
+        elements.forEach(element => {
+            if (ScrollContainerData.isValidHtmlElement(element)) {
+                element.classList.add(className);
+                element.scrollIntoView(ScrollContainerData.#scrollBehavior);
+            }
+        });
+    }
+
+    /**
+     * @param {NodeListOf<Element>} elements
+     * @param {string} className
+     */
+    removeClassFromElements(elements, className) {
+        elements.forEach(element => {
+            if (ScrollContainerData.isValidHtmlElement(element)) {
+                element.classList.remove(className);
+            }
+        });
+    }
+
+    /**
+     * @param {any} element
+     * @returns {boolean}
+     */
+    static isValidHtmlElement(element) {
+        return element !== null && element instanceof HTMLElement;
     }
 }
 
@@ -52,26 +117,20 @@ function handleScroll(event) {
     const scrollableHeight = mainScrollContainer.scrollHeight - mainScrollContainer.clientHeight;
     if (scrollableHeight > 0) {
         let scrollPercentage = (mainScrollContainer.scrollTop / scrollableHeight) * 100;
+        data.setBodyGridSize(scrollPercentage);
         if (isBetween(0, 33, scrollPercentage)) {
-            data.homeLink.classList.add(ScrollContainerData.activeClassName);
-            data.projectsLink.classList.remove(ScrollContainerData.activeClassName);
-            data.aboutMeLink.classList.remove(ScrollContainerData.activeClassName);
+            data.setHomeLinksToActive();            
         }
         else if (isBetween(33, 66, scrollPercentage)) {
-            data.homeLink.classList.remove(ScrollContainerData.activeClassName);
-            data.projectsLink.classList.add(ScrollContainerData.activeClassName);
-            data.aboutMeLink.classList.remove(ScrollContainerData.activeClassName);
+            data.setProjectsLinksToActive();
         }
         else {
-            data.homeLink.classList.remove(ScrollContainerData.activeClassName);
-            data.projectsLink.classList.remove(ScrollContainerData.activeClassName);
-            data.aboutMeLink.classList.add(ScrollContainerData.activeClassName);
+            data.setAboutMeLinksToActive();
         }
     }
 }
 
 /**
- * 
  * @param {number} min
  * @param {number} max
  * @param {number} num
@@ -79,6 +138,17 @@ function handleScroll(event) {
  */
 function isBetween(min, max, num) {
     return num >= min && num <= max;
+}
+
+/**
+ * @param {number} inputMin
+ * @param {number} inputMax
+ * @param {number} outputMin
+ * @param {number} outputMax
+ * @param {number} value
+ */
+function map(inputMin, inputMax, outputMin, outputMax, value) {
+    return outputMin + ((value - inputMin) * (outputMax - outputMin)) / (inputMax - inputMin);
 }
 
 async function downloadFileFromStream(fileName, contentStreamRef) {
